40bd92b94df4f4015da00952424d916572f242f1
Peter Aigner <mastapetz@icloud.com>
2022-07-07 12:17:45 +0200

    reports 1-4 done

--- Peter/sqlD3queries.sql
+++ Peter/sqlD3queries.sql
@@ -9,3 +9,17 @@ SELECT COUNT(*) FROM titles;
 SELECT COUNT(*) FROM `employees` WHERE first_name = "Mark"; 
 --How many employees with the first name "Eric" and the last name beginning with "A" do we have in our company?
 SELECT COUNT(*) FROM `employees` WHERE first_name = "Eric" AND last_name LIKE "A%"; 
+-- How many employees do we have that are working for us since 1985 and who are they?
+--only names no counting 
+SELECT first_name, last_name FROM `employees` WHERE `hire_date` like "1985%"
+--result
+   Showing rows 0 - 24 (35316 total, Query took 0.0139 seconds.)
+--names and counting
+SELECT count(*) from employees UNION SELECT `first_name` FROM `employees` WHERE `hire_date` like "1985%"; 
+--result
+   Showing rows 0 - 24 (1276 total, Query took 0.1506 seconds.)
+
+
+
+
+
