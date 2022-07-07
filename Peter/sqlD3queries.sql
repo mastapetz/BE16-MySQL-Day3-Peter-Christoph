4be09a7396f8440c8a9e2fcc237cbbd841ad6713
Peter Aigner <mastapetz@icloud.com>
2022-07-07 13:02:32 +0200

    commentary, rep 7 done

--- Peter/sqlD3queries.sql
+++ Peter/sqlD3queries.sql
@@ -1,15 +1,15 @@
---How many rows do we have in each table in the emplyees database
+-- Rep 1: How many rows do we have in each table in the emplyees database
 SELECT COUNT(*) FROM departments;
 SELECT COUNT(*) FROM dept_emp;
 SELECT COUNT(*) FROM dept_manager;
 SELECT COUNT(*) FROM employees;
 SELECT COUNT(*) FROM salaries;
 SELECT COUNT(*) FROM titles;
---How many employees with the first name "Mark" do we have in our company?
+-- Rep 2: How many employees with the first name "Mark" do we have in our company?
 SELECT COUNT(*) FROM `employees` WHERE first_name = "Mark"; 
---How many employees with the first name "Eric" and the last name beginning with "A" do we have in our company?
+-- Rep 3: How many employees with the first name "Eric" and the last name beginning with "A" do we have in our company?
 SELECT COUNT(*) FROM `employees` WHERE first_name = "Eric" AND last_name LIKE "A%"; 
--- How many employees do we have that are working for us since 1985 and who are they?
+-- Rep 4: How many employees do we have that are working for us since 1985 and who are they?
 --only names no counting 
 SELECT first_name, last_name FROM `employees` WHERE `hire_date` like "1985%"
 --result
@@ -18,6 +18,48 @@ SELECT first_name, last_name FROM `employees` WHERE `hire_date` like "1985%"
 SELECT count(*) from employees UNION SELECT `first_name` FROM `employees` WHERE `hire_date` like "1985%"; 
 --result
    Showing rows 0 - 24 (1276 total, Query took 0.1506 seconds.)
+-- Rep 5: How many employees got hired from 1990 until 1997 and who are they?
+SELECT COUNT(*) FROM `employees` WHERE hire_date > '1990-01-01' AND hire_date < '1997-12-31' UNION SELECT first_name FROM `employees`; 
+--result
+   Showing rows 0 - 24 (1276 total, Query took 0.2682 seconds.)
+-- Rep 6:How many employees have salaries higher than EUR 70 000,00 and who are they? 
+SELECT COUNT(*) FROM `employees` WHERE `emp_no` IN(SELECT emp_no from salaries WHERE salary > 70000); 
+--result (same result with DISTINCT)
+Your SQL query has been executed successfully.
+135631	
+--
+SELECT `first_name`,`last_name` FROM `employees` WHERE `emp_no` IN(SELECT DISTINCT emp_no from salaries WHERE salary > 70000);
+--result
+   Showing rows 0 - 24 (135631 total, Query took 0.0003 seconds.)
+-- Rep 7: How many employees do we have in the Research Department, who are working for us since 1992 and who are they?
+-- TIP: You can use the CURRENT_DATE() FUNCTION to access today's date
+--part1
+SELECT COUNT(*) FROM `dept_emp` WHERE `dept_no` = "d008"; 
+--full?
+SELECT COUNT(*) FROM `employees` WHERE `emp_no` IN(SELECT emp_no from dept_emp WHERE dept_no ="d008"); 
+--result
+Your SQL query has been executed successfully.
+21126	
+--
+SELECT `first_name`, `last_name` FROM `employees` WHERE `emp_no` IN(SELECT emp_no from dept_emp WHERE dept_no ="d008");
+--result
+   Showing rows 0 - 24 (21126 total, Query took 0.0058 seconds.)
+-- Rep 8: How many employees do we have in the Finance Department, who are working for us since 1985 until now and who have salaries higher than EUR 75 000,00 - who are they?
+
+-- Rep 9: We need a table with employees, who are working for us at this moment: first and last name, date of birth, gender, hire_date, title and salary.
+
+-- Rep 10:  We need a table with managers, who are working for us at this moment: first and last name, date of birth, gender, hire_date, title, department name and salary.
+
+-- Bonus:   Create a query that will join all tables and show all data from all tables.
+
+-- Next step:  Now you should create at least 5 queries on your own, try to use data from more than 2 tables.
+
+
+
+
+
+
+
 
 
 
